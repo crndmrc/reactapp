2d1077aeb9b3e55241e588535ee6c62508a1830d
crndmrc <ceren_57_99@hotmail.com>
2021-03-31 22:29:51 +0300

    Reactapp
    Todo App added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
+import ListItems from './ListItems';
+import ReactNotification from 'react-notifications-component'
+import {store} from "react-notifications-component"
+import 'animate.css/animate.min.css';
+import 'animate.css/animate.compat.css'
+import 'react-notifications-component/dist/theme.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.handleNoteInput=this.handleNoteInput.bind(this);
+    this.addItem=this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+  handleNoteInput(e){
+    this.setState({
+      currentItem:{
+        text:e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if(newItem !== ""){
+      const newItems=[...this.state.items,newItem];
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
+      this.setState({
+        items:newItems,
+        nameItem:{
+          text:'',
+          key:''
+        }
+      })
+    }
+  }
+  deleteItem(key){
+    const filteredItems= this.state.items.filter(item =>
+      item.key!==key);
+    this.setState({
+      items: filteredItems
+    })
+  }
+  render() {
+    const handleOnClickDefault =()=> {
+      store.addNotification({
+        title:"--Mesaj Bildirimi--",
+        message:"Başarılı bir şekilde eklendi!!",
+        type:"success",
+        container:"bottom-left",
+        insert:"bottom",
+        animationIn: ["animate__animated animate__fadeIn"],
+        animationOut: ["animate__animated animate__fadeOut"],
+        dismiss:{
+          duration:3000,
+          showIcon:true
+        },
+        width:800
+      })
+    }
+    function btnOnClickVisibiltyNone (){
+
+    }
+    return (
+      <div className="App">
+        <form id="to-do-form" onSubmit={this.addItem}>
+            <h4>To Do List</h4>
+            <label htmlFor="name">Name</label>
+            <input
+            id="name"
+            type="text"
+            placeholder="Enter Name"
+            />
+            <label htmlFor="lastname">Lastname</label>
+            <input
+            id="lastname"
+            type="text"
+            placeholder="Enter Lastname"
+            />
+            <hr/>
+            <input 
+            type="text" 
+            placeholder="Enter Note"
+            value={this.state.currentItem.text}
+            onChange={this.handleNoteInput}
+            />
+            <button onClick={handleOnClickDefault,btnOnClickVisibiltyNone} type="submit">Add List</button>  
+            <ReactNotification/>
+        </form>
+        <hr/>
+        <ListItems items={this.state.items} deleteItem={this.deleteItem}/>
+        <footer>
+            Design by Ceren
+        </footer>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/ListItems.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './ListItem.css';
+
+function ListItems (props) {
+    const items = props.items;
+    const listitems = items.map(item=>{
+        return   <div className="draggable" draggable="true" key="item.key">
+        <span>{item.text}<button id="btndelete" onClick={() => {
+            props.deleteItem(item.key)}} className="btndelete">X</button><button id="changeColor" onClick={()=>{
+            document.getElementsByClassName("draggable")[0].style.background="#FF5733";
+            document.getElementById("changeColor").style.visibility="hidden";
+        }}>✓</button></span>
+        </div>
+    })
+    const draggables =document.querySelectorAll('.draggable')
+    const contaniers = document.querySelectorAll('.contanier')
+    draggables.forEach(draggable=>{
+        draggable.addEventListener('dragstart',()=>{
+            draggable.classList.add('dragging')
+        })
+    draggable.addEventListener('dragend',()=>{
+        draggable.classList.remove('dragging')
+    })
+    contaniers.forEach(contanier=>{
+        contanier.addEventListener('dragover',e=>{
+            e.preventDefault()
+        const afterElement =getDragAfterElement(contanier,e.clientY)
+        console.log(afterElement)
+        const draggable = document.querySelector('.dragging')
+        if (afterElement == null){
+            contanier.appendChild(draggable)
+        }
+        else {
+            contanier.insertBefore(draggable,afterElement)
+        }
+        contanier.appendChild(draggable)
+        })
+    })
+})
+function getDragAfterElement(contanier,y){
+    const draggableElements = [...contanier.querySelectorAll('draggable:not(.draggable)')]
+    return draggableElements.reduce((closest,child)=>{
+    const box=child.getBoundingClientRect()
+    const offset = y - box.top - box.height /2 
+    console.log(offset)
+    if(offset>0 && offset > closest.offset){
+        return {offset: offset,element: child}
+    }
+    else {
+        return closest    
+    }
+    },{offset:Text.NEGATIVE_INFINITY}).element
+}
+    return (
+    <div className="contanier"> 
+        {listitems}
+    </div>
+    )
+}
+export default ListItems
